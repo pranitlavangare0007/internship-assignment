611a1a0fb97fabc7b3439ff25e0d8f8ac363e65a
Pranit Lavangare <pranitlavangare0007@gmail.com>
2026-04-20 13:16:49 +0530

    started assignment and connected app with redis

---
+++ src/main/java/com/pranit/assignment/TestController.java
@@ -0,0 +1,19 @@
+package com.pranit.assignment;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    @GetMapping("/test-redis")
+    public String testRedis() {
+        redisTemplate.opsForValue().set("test:key", "Hello Redis Cloud");
+        return redisTemplate.opsForValue().get("test:key");
+    }
+}
---
+++ src/test/java/com/pranit/assignment/AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranit.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
